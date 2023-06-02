7b3b66f714dc22508ca7ac09b47eb393abd2e13a
John Chacpi <johnchacpi479@gmail.com>
2023-06-01 20:54:18 -0500

    added environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    producution: false,
+    weatherLocationBaseApiURL: "https://foreca-weather.p.rapidapi.com/location/search/",
+    weatherDataBaseApiUrl: "https://foreca-weather.p.rapidapi.com/current/",
+    XRapidAPIHostHeaderName: "X-RapidAPI-Key",
+    XRapidAPIHostHeaderValue: "foreca-weather.p.rapidapi.com",
+    XRapidAPIKeyHeaderName: "X-RapidAPI-Host",
+    XRapidAPIKeyHeaderValue: "f795c979d9msh737fce69d2d0e2ap140ed4jsn705b07bcd026",
+
+}
